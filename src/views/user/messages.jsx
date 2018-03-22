62e60411b7100a17a0ce99d4b92ebc300580cfe4
Z-wave <s.wave@qq.com>
2018-03-22 21:56:37 +0800

    class

--- src/views/user/messages.jsx
+++ src/views/user/messages.jsx
@@ -15,20 +15,20 @@ class App extends React.Component {
         return (
             <div id="wrapper" className="spacing">
                 <Header title="消息" leftTo="/" />
-                <div class="box box-items profile p10 border-bottom">
-                    <div class="pic">
+                <div className="box box-items profile p10 border-bottom">
+                    <div className="pic">
                         <img src="https://avatars1.githubusercontent.com/u/22312483?v=4&amp;s=120" alt="" />
                     </div>
-                    <div class="flex-1 ml5">
+                    <div className="flex-1 ml5">
                         <p><span className="fs16">xxx</span> <span className="color-9">1天前</span></p>
                         <p>回复了你的话题<span className="color">这个新话题</span></p>
                     </div>
                 </div>
-                <div class="box box-items profile p10 border-bottom">
-                    <div class="pic">
+                <div className="box box-items profile p10 border-bottom">
+                    <div className="pic">
                         <img src="https://avatars1.githubusercontent.com/u/22312483?v=4&amp;s=120" alt="" />
                     </div>
-                    <div class="flex-1 ml5">
+                    <div className="flex-1 ml5">
                         <p><span className="fs16">xxx</span> <span className="color-9">1天前</span></p>
                         <p>在话题<span className="color">这个新话幽幽幽幽经理人题</span>@了你</p>
                     </div>
